a516d7f36e94771183764017c049fe01ed3d176a
Barak Aharoni <97598628+BarakAharoni@users.noreply.github.com>
2022-01-25 20:08:19 +0200

    Add files via upload

---
+++ Scanner/ScannerWPF/Address.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerWPF
+{
+    public class Address
+    {
+        private string mac;
+        private string ip;
+
+        public Address(string ip, string mac)
+        {
+            this.ip = ip;
+            this.mac = mac;
+        }
+
+        public string Ip
+        {
+            get { return this.ip; }
+            set { this.ip = value; }
+        }
+
+        public string Mac
+        {
+            get { return this.mac; }
+            set { this.mac = value; }
+        }
+
+        public override string ToString()
+        {
+            return "IP: " + this.ip + "------> MAC: " + this.mac;
+        }
+    }
+}
